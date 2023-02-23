f81a846a27d598e0b202a85f3ea755edc9c1cfcf
Vishwanath-123 <m.vishwanath2003@gmail.com>
2023-02-23 19:43:57 +0530

    commit

--- presentation_1/main.R
+++ presentation_1/main.R
@@ -3,7 +3,7 @@ install.packages("stats")
 
 library(stats)
 library(dplyr)
-db <- read.csv("Presentation_1/Country_data2.csv",  header = TRUE)
+db <- read.csv("presentation_1/Country_data2.csv",  header = TRUE)
 View(db)
 dim(db)
 main_data <- db
@@ -18,14 +18,21 @@ View(cor(select(main_data, -c(country))))
 avg <- mean(cor(select(main_data, -c(country))))
 
 # PCA
-pca <- prcomp(select(main_data, -c(country)), center = TRUE,   scale. = TRUE)
+pca <- princomp(select(main_data, -c(country)), center = TRUE,   scale. = TRUE)
+pca$loadings
+
+pc <- pca$scores
+View(pc)
+
+View(cor(pc))
+
 summary(pca)
 th <- 4
 eigen_val <- pca$sdev^2
 eigen_val
 
 # Plotting the Eigen Values
-jpeg(file = "Presentation-1/images/pca.jpeg")
+jpeg(file = "presentation-1/images/pca.jpeg")
 plot(eigen_val, type = "o", xlab = "Number of components", ylab = "Eigen Values", col = "brown") 
 abline(h = eigen_val[th], col = "red", lty = 100)
 dev.off()
@@ -34,12 +41,12 @@ cumprop <- cumsum(eigen_val) / sum(eigen_val)
 cumprop
 
 # Plotting the Cumulative Proportion
-jpeg(file = "Presentation-1/images/Cummulative_Proportion.jpeg")
+jpeg(file = "presentation-1/images/Cummulative_Proportion.jpeg")
 plot(cumprop * 100,type = "b",xlab = "Number of components", ylab = "Cumulative Proportion of Variance", col = "blue")
 abline(v = th, col = "red", lty = 2)
 abline(h = cumprop[th] * 100, col = "red", lty = 2)
 dev.off()
 
-jpeg(file = "Presentation-1/images/.jpeg")
+jpeg(file = "presentation-1/images/.jpeg")
 plot(pca,  type = "l")
 loadings(select(pca, c(1:th)))
