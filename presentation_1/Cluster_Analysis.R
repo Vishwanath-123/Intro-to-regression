f1d9d4d06f73f63551d0188bc6b1791cd2e9021e
Vishwanath-123 <m.vishwanath2003@gmail.com>
2023-03-01 20:48:14 +0530

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- presentation_1/Cluster_Analysis.R
+++ presentation_1/Cluster_Analysis.R
@@ -11,22 +11,22 @@ main_data["exports"] <- main_data["exports"] * main_data["gdpp"] / 100
 main_data["imports"] <- main_data["imports"] * main_data["gdpp"] / 100
 main_data["health"] <- main_data["health"] * main_data["gdpp"] / 100
 
-# wssplot <- function(data, nc = 15, seed = 1234){
-#   wss <- (nrow(data)-1)*sum(apply(data,2,var))
-#   for (i in 2:nc){
-#     wss[i] <- sum(kmeans(data, centers = i)$withinss)
-#   }
-#   plot(1:nc, wss, type = "b", xlab  =  "Number of Clusters",
-#        ylab = "Within groups sum of squares")
-# }
-# pdf("presentation_1/images/wssplot.pdf")
-# wssplot(main_data)
-# dev.off()
+wssplot <- function(data, nc = 15, seed = 1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc){
+    wss[i] <- sum(kmeans(data, centers = i)$withinss)
+  }
+  plot(1:nc, wss, type = "b", xlab  =  "Number of Clusters",
+       ylab = "Within groups sum of squares")
+}
+pdf("presentation_1/images/wssplot.pdf")
+wssplot(main_data)
+dev.off()
 
 number_of_clusters <- 3
 KM <- kmeans(main_data, number_of_clusters)
 
-pdf("presentation_1/images/CA3.pdf")
+pdf("presentation_1/images/CA.pdf")
 autoplot(KM, main_data, frame = TRUE)
 dev.off()
 
@@ -35,6 +35,6 @@ dev.off()
 hc <- hclust(dist(main_data), method = "ward.D")
 dend_obj <- as.dendrogram(hc)
 col_obj <- color_branches(dend_obj, k = number_of_clusters)
-pdf("presentation_1/images/dendogram3.pdf")
+pdf("presentation_1/images/dendogram.pdf")
 plot(col_obj)
 dev.off()
